a7ce29beb2d9e5af2eca6ca42ea390886e8a4011
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-07-28 23:54:09 +0700

    Update Path, Frame Skip

--- poc/lcp_vid.py
+++ poc/lcp_vid.py
@@ -4,20 +4,27 @@ import time
 import os
 
 def init_dir(output_dir):
+    print("Initing Dir: "+ output_dir)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+        print("Successful Init: "+ output_dir)
+    else:
+        print(output_dir + " is exist.")
 
 def main():
+    # Init dir
     output_dir = 'lcp-images'
     init_dir(output_dir)
 
+
     frame_count = 0
+    frame_skip = 5
 
     # Load the pre-trained Haar Cascade classifier for Russian car plates
-    carPlatesCascade = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
+    carPlatesCascade = cv2.CascadeClassifier('haarcascade/haarcascade_russian_plate_number.xml')
 
     # Open the video file
-    cap = cv2.VideoCapture('data/5.mp4')
+    cap = cv2.VideoCapture('test-data/5.mp4')
 
     # Set frame width and height
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
@@ -40,24 +47,27 @@ def main():
         if not ret:
             break
 
-        # Convert the frame to grayscale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Skip frames to reduce the rate of capture
+        if frame_count % frame_skip == 0:
+            # Convert the frame to grayscale
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
-        # Detect car plates in the frame
-        plate = carPlatesCascade.detectMultiScale(gray, scaleFactor=1.4, minNeighbors=2)
+            # Detect car plates in the frame
+            plates = carPlatesCascade.detectMultiScale(gray, scaleFactor=1.4, minNeighbors=2, minSize=(30, 30))
 
-        if len(plate) > 0:
-            # Draw rectangles around detected car plates
-            for (x, y, w, h) in plate:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
-            
-            # Save the frame with rectangles to the specified folder
-            frame_path = os.path.join(output_dir, f'frame_{frame_count}.jpg')
-            cv2.imwrite(frame_path, frame)
-            frame_count += 1
-            
+            if len(plates) > 0:
+                # Draw rectangles around detected car plates
+                for (x, y, w, h) in plates:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+
+                # Save the frame with rectangles to the specified folder
+                frame_path = os.path.join(output_dir, f'frame_{frame_count}.jpg')
+                cv2.imwrite(frame_path, frame)
+
         # Display the resulting frame
         cv2.imshow('Cars', frame)
+        frame_count += 1
 
         # Break loop on 'Enter' key press
         if cv2.waitKey(1) == 13:
